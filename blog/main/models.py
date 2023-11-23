b6fead9ff32ddeb6720d9c6892186c661ba0b3ce
Vyacheslav <polyerter@gmail.com>
2023-11-23 16:04:07 +0700

    added

--- blog/main/models.py
+++ blog/main/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
+
 class Article(models.Model):
-     title = models.CharField(max_length=255)
-     text = models.TextField()
-     created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
